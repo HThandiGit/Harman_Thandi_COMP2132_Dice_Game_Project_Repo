82a5506aa1c0afb57daec7eecd5e989148794590
HThandiGit <86066676+HThandiGit@users.noreply.github.com>
2021-06-17 18:27:24 -0700

    using OOP and testing SASS
    Tested SASS to make sure the extension is working and made a basic implementation of the Dice class I am going to use.

--- Scripts/Dice.js
+++ Scripts/Dice.js
@@ -1,1 +1,29 @@
-class Dice{}
+class Dice{
+
+    constructor(firstDieValue, secondDieValue){
+
+        this.firstDieValue = firstDieValue;
+        this.secondDieValue = secondDieValue;
+    }
+
+    rollDice(){
+
+        this.firstDieValue = Math.floor((Math.random() * 6) + 1);
+        this.secondDieValue = Math.floor((Math.random() * 6) + 1);
+    }
+
+    getFirstDieVal(){
+
+        return this.firstDieValue;
+    }
+
+    getSecondDieVal(){
+
+        return this.secondDieValue;
+    }
+
+    getDiceSum(){
+
+        return (this.firstDieValue + this.secondDieValue);
+    }
+}    
